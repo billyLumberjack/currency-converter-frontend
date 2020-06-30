70955fba33a3166379fafef58e046d107973a501
samuele.malavasi <samuele.malavasi@gmail.com>
2020-06-30 15:38:47 +0200

    Added AuthGuard to redirect unauthenticated users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
-import { AuthenticationService } from '@app/services/authentication.service';
+import { AuthGuard } from '@app/helpers/auth.guard';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthenticationService] },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' }
 ];
